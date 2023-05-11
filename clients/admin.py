fee98361b8636545fe24cfeeea06624166a834b3
Abdullah Joud <joud@kfupm.edu.sa>
2023-05-11 13:42:17 +0300

    customize admin pages for all models in clients app

--- clients/admin.py
+++ clients/admin.py
@@ -2,6 +2,44 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Organization)
-admin.site.register(Department)
-admin.site.register(Employee)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name_ar',
+        'name_en',
+        'type',
+        'city',
+    )
+
+    list_filter = (
+        'type',
+        'city',
+    )
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'organization',
+        'title_ar',
+        'title_en',
+    )
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name_en',
+        'last_name_en',
+        'job_title',
+        'department',
+        'nationality',
+        'mobile',
+    )
+
+    list_filter = (
+        'job_title',
+        'nationality',
+    )
+
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Employee, EmployeeAdmin)

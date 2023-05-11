8e0b784a496ee291ef8fa129ae912c1fb8fc379d
Abdullah Joud <joud@kfupm.edu.sa>
2023-05-11 10:56:38 +0300

    define admin classes for all models in assessment app

--- assessment/admin.py
+++ assessment/admin.py
@@ -2,16 +2,85 @@ from django.contrib import admin
 from .models import *
 
 
-class SectionAdmin(admin.ModelAdmin):
+class AssessmentAdmin(admin.ModelAdmin):
     list_filter = (
+        'type',
+    )
+
+    list_display = (
+        'title_ar',
+        'title_en',
+        'type',
+        'derived_from',
+        'created_by',
+        'created_on',
+    )
+
+    date_hierarchy = 'created_on'
+
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display = (
         'title_ar',
         'title_en',
         'sub_of',
+        'display_order',
+        'show_flag',
+    )
+
+    list_filter = (
+        'show_flag',
     )
 
 
-admin.site.register(Assessment)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        'assessment',
+        'prompt_ar',
+        'prompt_en',
+        'section',
+        'display_order',
+    )
+
+    list_filter = (
+        'section',
+    )
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        'question',
+        'prompt_ar',
+        'prompt_en',
+        'selected_answer',
+        'weight',
+        'answered_by',
+    )
+
+    list_filter = (
+        'selected_answer',
+    )
+
+    date_hierarchy = 'answered_on'
+
+
+class EvidenceAdmin(admin.ModelAdmin):
+    list_display = (
+        'question',
+        'type',
+        'uploaded_file',
+        'uploaded_by',
+    )
+
+    list_filter = (
+        'type',
+    )
+
+    date_hierarchy = 'uploaded_on'
+
+
+admin.site.register(Assessment, AssessmentAdmin)
 admin.site.register(Section, SectionAdmin)
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(Evidence)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Evidence, EvidenceAdmin)

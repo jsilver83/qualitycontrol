b942b6b23bb0e42cd05b5100be98a78ba6c4f52c
Abdullah Joud <joud@kfupm.edu.sa>
2023-05-11 10:01:32 +0300

    add basic admin for assessment models

--- assessment/admin.py
+++ assessment/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class SectionAdmin(admin.ModelAdmin):
+    list_filter = (
+        'title_ar',
+        'title_en',
+        'sub_of',
+    )
+
+
+admin.site.register(Assessment)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Evidence)

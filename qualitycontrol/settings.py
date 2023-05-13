cce1f7c72e5ce230c2d95de087a107f11ddce49a
Abdullah Joud <joud@kfupm.edu.sa>
2023-05-13 21:39:39 +0300

    add local_settings support

--- qualitycontrol/settings.py
+++ qualitycontrol/settings.py
@@ -178,3 +178,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('local_settings file not found')

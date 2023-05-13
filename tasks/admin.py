d195181bceca4769f2c988dfb3c64d34047cd632
Abdullah Joud <joud@kfupm.edu.sa>
2023-05-13 21:44:01 +0300

    customize task model in admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        'description_ar',
+        'description_en',
+        'question',
+        'due_date',
+        'assigned_employee',
+        'completion_date',
+        'completed_by',
+    ]
+
+    date_hierarchy = 'created_on'
+
+
+admin.site.register(Task, TaskAdmin)

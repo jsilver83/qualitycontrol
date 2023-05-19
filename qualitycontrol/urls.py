194e34b8d8d3583a03e0c1327de84aafd7c7ad1d
Abdullah Joud <joud@kfupm.edu.sa>
2023-05-19 17:08:23 +0300

    add urls for MEDIA_URL and MEDIA_ROOT in debug mode

--- qualitycontrol/urls.py
+++ qualitycontrol/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -27,3 +29,6 @@ urlpatterns = i18n_patterns(
 urlpatterns += [
     path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
